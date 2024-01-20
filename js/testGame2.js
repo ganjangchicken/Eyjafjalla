4194d246f7687bc565295425a0601aff0d268009
ganjangchicken <146302394+ganjangchicken@users.noreply.github.com>
2024-01-21 04:30:48 +0900

    middle save

--- js/testGame2.js
+++ js/testGame2.js
@@ -1,21 +1,27 @@
+const LEFT_AIM = 0;
+const RIGHT_AIM = 1;
+
 class Example extends Phaser.Scene
 {
-    scoreText;
     gameOver = false;
     score = 0;
     cursors;
     platforms;
-    bombs;
-    stars;
     player;
+    player_sight = RIGHT_AIM;
+    boss;
+    bgm;
+    hpText = 1600;
 
     preload ()
     {
-        this.load.spritesheet('HoneyBerry', '/img/HoneyBerry/test2.png', { frameWidth: 800, frameHeight: 800 });
-        
+        this.load.spritesheet('HoneyBerry', '/img/HoneyBerry/HoneyBerrySprite.png', { frameWidth: 400, frameHeight: 400 });
+        this.load.spritesheet('Eyjafjalla', '/img/Eyjafjalla/EyjafjallaSpriteSheet.png', {frameWidth: 800, frameHeight: 800});
+
         this.load.image('sky', 'https://labs.phaser.io/src/games/firstgame/assets/sky.png');
         this.load.image('ground', 'https://labs.phaser.io/src/games/firstgame/assets/platform.png');
         
+        this.load.audio('mainBgm', '/audio/CODE_NAME_GAMMA.mp3');
         //this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
         
 
@@ -34,44 +40,110 @@ class Example extends Phaser.Scene
         this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
         //  Now let's create some ledges
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
+        this.platforms.create(600, 450, 'ground').setScale(0.7,1).refreshBody();
+        this.platforms.create(200, 450, 'ground').setScale(0.7,1).refreshBody();
+        this.platforms.create(400, 350, 'ground').setScale(0.5,1).refreshBody();
+        this.platforms.create(-50, 350, 'ground').setScale(0.7,1).refreshBody();
+        this.platforms.create(850, 350, 'ground').setScale(0.7,1).refreshBody();
+
+        
 
         // The player and its settings
-        //this.player = this.physics.add.sprite(100, 450, 'dude');
-        this.player = this.physics.add.sprite(100, 100, 'HoneyBerry').setScale(0.2);
+        this.player = this.physics.add.sprite(100, 450, 'HoneyBerry').setScale(0.2);
+        this.boss = this.physics.add.sprite(400, 500, 'Eyjafjalla').setScale(0.2).body.setAllowGravity(false);
 
         //  Player physics properties. Give the little guy a slight bounce.
-        // this.player.setBounce(0.2);
-        // this.player.setCollideWorldBounds(true);
-        this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
+        this.player.setBodySize(100, 300);
 
         //  Our player animations, turning, walking left and walking right.
         this.anims.create({
             key: 'idle',
-            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 0, end: 26 }),
-            frameRate: 3,
+            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 27, end: 157}),
+            frameRate: 60,
             repeat: -1
         });
 
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [ { key: 'dude', frame: 4 } ],
-        //     frameRate: 20
-        // });
+        this.anims.create({
+            key: 'attack',
+            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 0, end: 26 }),
+            frameRate: 60,
+            repeat: -1
+        });
 
-        // this.anims.create({
-        //     key: 'right',
-        //     frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
+        this.anims.create({
+            key: 'move_left',
+            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 158, end: 178 }),
+            frameRate: 60,
+            repeat: -1
+        });
 
+        this.anims.create({
+            key: 'move_right',
+            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 179, end: 199 }),
+            frameRate: 60,
+            repeat: -1
+        });
+
+        this.anims.create({ // 38
+            key: 'jump_left',
+            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 199, end: 236 }),
+            frameRate: 60,
+            repeat: -1
+        });
+
+        this.anims.create({ // 38
+            key: 'jump_right',
+            frames: this.anims.generateFrameNumbers('HoneyBerry', { start: 237, end: 274 }),
+            frameRate: 60,
+            repeat: -1
+        });
+
+        //Eyjafjalla ani - 87 idle/ 12 begin/ 10 end/ 87 idle3/ 93 loop
+        this.anims.create({ 
+            key: 'idle3',
+            frames: this.anims.generateFrameNumbers('Eyjafjalla', { start: 109, end: 194 }),
+            frameRate: 60,
+            repeat: -1
+        });
+
+        this.anims.create({ 
+            key: 'loop',
+            frames: this.anims.generateFrameNumbers('Eyjafjalla', { start: 195, end: 288 }),
+            frameRate: 60,
+            repeat: -1
+        });
+
+        this.anims.create({ 
+            key: 'begin',
+            frames: this.anims.generateFrameNumbers('Eyjafjalla', { start: 87, end: 98 }),
+            frameRate: 60,
+            repeat: 1
+        });
+
+        this.anims.create({ 
+            key: 'idleE',
+            frames: this.anims.generateFrameNumbers('Eyjafjalla', { start: 0, end: 86 }),
+            frameRate: 60,
+            repeat: -1
+        });
+        
+
+        //  Input Events
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         //  Collide the player and the stars with the platforms
         this.physics.add.collider(this.player, this.platforms);
+
+        // bgm start
+        this.bgm = this.sound.add('mainBgm');
+        this.bgm.play();
+
+        // boss patern
+        this.boss.gameObject.anims.play('idleE', true);
+        setTimeout(e => {
+            
+        }, 18000);
 
     }
 
@@ -81,30 +153,45 @@ class Example extends Phaser.Scene
         {
             return;
         }
+        
+        //console.dir(this.boss);
+        // console.dir(this.player);
 
-        // if (this.cursors.left.isDown)
-        // {
-        //     this.player.setVelocityX(-160);
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-160);
+            
+            this.player.anims.play('move_left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(160);
+            
+            this.player.anims.play('move_right', true);
+        }
+        else
+        {
+            this.player.setVelocityX(0);
 
-        //     this.player.anims.play('idle', true);
-        // }
-        // else if (this.cursors.right.isDown)
-        // {
-        //     this.player.setVelocityX(160);
+            this.player.anims.play('idle', true);
+        }
 
-        //     this.player.anims.play('right', true);
-        // }
-        // else
-        // {
-        //     this.player.setVelocityX(0);
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-630);
+        }
+        
+        if(this.boss.gameObject.y > 100 && this.bgm.seek > 0){
+            this.boss.gameObject.y = this.boss.gameObject.y - 0.2;
+            this.boss.gameObject.anims.play('idle3', true);
+        }
 
-        //     this.player.anims.play('turn');
-        // }
+        if(this.bgm.seek > 18) {
+            this.boss.gameObject.anims.play('loop', true);
+        }
 
-        // if (this.cursors.up.isDown && this.player.body.touching.down)
-        // {
-        //     this.player.setVelocityY(-330);
-        // }
+        //console.log(this.bgm.seek);
+        
     }
 
 
@@ -117,8 +204,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
-            debug: false
+            gravity: { y: 1500 },
+            debug: true
         }
     },
     scene: Example
